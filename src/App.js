ba57991f0236c67e84bc11e68511c12bd84e5d3d
Olivier-Kango <olivierkango@gmail.com>
2022-11-07 18:46:16 -0800

    Add App Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import MyComponent from './components/Greet';
+import Welcome from './components/Welcome';
+import Hello from './components/Hello';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render () {
+    return (
+      <div className="App">
+        {/* <MyComponent /> */}
+        {/* <Welcome /> */}
+        <Hello />
+      </div>
+    );
+  }
 }
 
 export default App;
