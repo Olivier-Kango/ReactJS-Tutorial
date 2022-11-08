a18bf161ecac24c906b2032474991aaad5146776
Olivier-Kango <olivierkango@gmail.com>
2022-11-07 19:41:02 -0800

    Add Greet and Welcome

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import MyComponent from './components/Greet';
+import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Hello from './components/Hello';
 
@@ -8,9 +8,17 @@ class App extends React.Component {
   render () {
     return (
       <div className="App">
-        {/* <MyComponent /> */}
-        {/* <Welcome /> */}
-        <Hello />
+        <Greet name="Bruce" heroName="Batman">
+          <p>This is children props</p>
+        </Greet>
+        <Greet name="Clark" heroName="Superman">
+          <button>Action</button>
+        </Greet>
+        <Greet name="Diana" heroName="Wonder Woman" />
+        <Welcome name="Bruce" heroName="Batman" />
+        <Welcome name="Clark" heroName="Superman" />
+        <Welcome name="Diana" heroName="Wonder Woman" />
+        {/* <Hello /> */}
       </div>
     );
   }
