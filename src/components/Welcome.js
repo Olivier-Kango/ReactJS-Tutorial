bfa4d6f9f871a24a72c13b80dd46fb4c62527ad4
Olivier-Kango <olivierkango@gmail.com>
2022-11-07 19:42:54 -0800

    Add this.state to the component

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,8 +1,12 @@
 import React from "react";
 
 class Welcome extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state={ name: 'Olivier' }
+  }
   render() {
-    return <h1>Class Component</h1>
+    return <h1>Welcome {this.state.name} and {this.props.name} a.k.a {this.props.heroName}</h1>
   }
 }
 
