bea09e4952c9820da510e778812323a3c737b5e7
Olivier-Kango <olivierkango@gmail.com>
2022-11-07 18:46:52 -0800

    Add Hello Component

---
+++ src/components/Hello.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Hello = () => {
+  // return (
+  //   <div className: 'dummyClass'>
+  //     <h1>Hello Vishwas</h1>
+  //   </div>
+  // )
+  return React.createElement(
+    'div', 
+    {id: 'hello', className: 'dummyClass'},
+    React.createElement('h1', null, 'Hello Vishwas'))
+}
+
+export default Hello;
